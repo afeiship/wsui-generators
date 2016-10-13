944bbb427bfb7edf9fe794966b0a7796e6abbbe5
feizheng <1290657123@qq.com>
2016-10-13 16:50:49 +0800

    update color & bgcolor

---
+++ gulp/styles.js
@@ -0,0 +1,19 @@
+(function() {
+
+  'use strict';
+
+  var gulp = require('gulp');
+  var config = require('./config');
+  var argv = require('yargs').argv;
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  //styles
+  gulp.task('styles',function() {
+    return gulp.src('src/**')
+      .pipe($.concat('webkit-sass-generator.scss'))
+      .pipe(gulp.dest('dist'));
+  });
+
+}());
---
+++ gulp/test.js
@@ -0,0 +1,19 @@
+(function() {
+
+  'use strict';
+
+  var gulp = require('gulp');
+  var config = require('./config');
+  var argv = require('yargs').argv;
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  //styles
+  gulp.task('test',function() {
+    return gulp.src('test/**')
+      .pipe($.sass({outputStyle: 'expanded'}).on('error', $.sass.logError))
+      .pipe(gulp.dest('test'));
+  });
+
+}());
