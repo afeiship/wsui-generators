f081698a368bad16a90e825722b25a01c7accf3a
afeiship <1290657123@qq.com>
2017-05-11 19:19:59 +0800

    udpate px->rem

--- gulp/styles.js
+++ gulp/styles.js
@@ -11,7 +11,22 @@
 
   //styles
   gulp.task('styles',function() {
-    return gulp.src('src/**')
+    return gulp.src([
+      'src/_init.scss',
+      'src/_background-color.scss',
+      'src/_blank.scss',
+      'src/_border-color.scss',
+      'src/_border-radius.scss',
+      'src/_color.scss',
+      'src/_font-size.scss',
+      'src/_grid.scss',
+      'src/_line.scss',
+      'src/_line-clamp.scss',
+      'src/_line-height.scss',
+      'src/_margin.scss',
+      'src/_padding.scss',
+      'src/_width.scss'
+    ])
       .pipe($.concat('webkit-sass-generators.scss'))
       .pipe(gulp.dest('dist'));
   });
