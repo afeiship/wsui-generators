55c7c24959e66d6040bcf53b89df2ed28ce68e2d
afeiship <1290657123@qq.com>
2018-03-08 11:41:52 +0800

    issue: rename

---
+++ build/styles.js
@@ -0,0 +1,35 @@
+(function() {
+
+  'use strict';
+
+  var gulp = require('gulp');
+  var config = require('./config');
+  var argv = require('yargs').argv;
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  //styles
+  gulp.task('styles',function() {
+    return gulp.src([
+      'src/init.scss',
+      'src/background-color.scss',
+      'src/blank.scss',
+      'src/border-color.scss',
+      'src/border-radius.scss',
+      'src/box.scss',
+      'src/color.scss',
+      'src/font-size.scss',
+      'src/grid.scss',
+      'src/line.scss',
+      'src/line-clamp.scss',
+      'src/line-height.scss',
+      'src/margin.scss',
+      'src/padding.scss',
+      'src/width.scss'
+    ])
+      .pipe($.concat('webkit-sass-generators.scss'))
+      .pipe(gulp.dest('dist'));
+  });
+
+}());
