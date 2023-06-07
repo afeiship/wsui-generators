a40577f359d11ac6c2e925cff6eb9f3fa0cba096
afeiship <1290657123@qq.com>
2023-06-07 20:53:21 +0800

    chore: gulp-sass

--- build/styles.js
+++ build/styles.js
@@ -1,21 +1,19 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var gulp = require('gulp');
-  // var sass = require('gulp-sass')(require('sass'));
-  var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*']
-  });
+const gulp = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+const $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*']
+});
 
-  //styles
-  gulp.task(
-    'styles',
-    gulp.parallel(function () {
-      return gulp
-        .src(['src/basic/*.scss', 'src/functions/*.scss', 'src/modules/*.scss'])
-        .pipe($.concat('index.scss'))
-        .pipe($.jswork.pkgHeader())
-        .pipe(gulp.dest('dist'));
-    })
-  );
-})();
+//styles
+gulp.task(
+  'styles',
+  gulp.parallel(function () {
+    return gulp
+      .src(['src/basic/*.scss', 'src/functions/*.scss', 'src/modules/*.scss'])
+      .pipe($.concat('index.scss'))
+      .pipe($.jswork.pkgHeader())
+      .pipe(gulp.dest('dist'));
+  })
+);
